40c95bb6a3076041a0d7874546f4d17067c88958
Jaime Diaz <jdiaz.detec@gmail.com>
2025-05-18 18:20:43 +0200

    bootstrap setup

--- tf/envs/0_base_setup/main.tf
+++ tf/envs/0_base_setup/main.tf
@@ -7,27 +7,9 @@ terraform {
   }
 }
 
-provider "google" {
-  project = local.envs["PROJECT_ID"]
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
 resource "google_project_iam_member" "editor" {
-    project = local.envs["PROJECT_ID"]
+    project = local.envs.PROJECT_ID
     role    = "roles/editor"
-    member  = "user:${local.envs["MAIL"]}"
+    member  = "user:${local.envs.MAIL}"
 }
 
-resource "google_project_iam_policy" "restrict_policy" {
-    project = local.envs["PROJECT_ID"]
-
-    policy_data = jsonencode({
-        bindings = [
-            {
-                role    = "roles/editor"
-                members = ["user:${local.envs["MAIL"]}"]
-            }
-        ]
-    })
-}
